8a251cbf436b3899178b1f092381f83d71590acf
JSCoutu <151463469+JSCoutu@users.noreply.github.com>
2024-07-15 12:12:21 -0400

    Ex2Requis4Pass

--- greeting.js
+++ greeting.js
@@ -2,6 +2,11 @@ function greet(name){
     if (name === undefined || name === null || name === ""){
         return "Hello there";
     }
+    else if(Array.isArray(name)){
+        if (name.length <= 2){
+        return "Hello, " + name[0] + " and " + name[1];
+        }
+    }
     else if(name === name.toUpperCase() ){
         return "HELLO " + name;
     }
