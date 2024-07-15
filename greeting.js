d614e99201992233d1c7af53ae1dd0c237ce2e78
JSCoutu <151463469+JSCoutu@users.noreply.github.com>
2024-07-15 19:23:04 -0400

    Ex2Requis5Pass

--- greeting.js
+++ greeting.js
@@ -6,6 +6,13 @@ function greet(name){
         if (name.length <= 2){
         return "Hello, " + name[0] + " and " + name[1];
         }
+        else {
+            let result = "Hello";
+            for(i=0;i<name.length - 1;i++){
+               result += ", " + name[i]
+            }
+            return result + " and " + name[name.length-1];
+        }
     }
     else if(name === name.toUpperCase() ){
         return "HELLO " + name;
