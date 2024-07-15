824d06605ade0e15e5ca5b0e9686d79f1f28048b
JSCoutu <151463469+JSCoutu@users.noreply.github.com>
2024-07-15 09:28:23 -0400

    requis1<>manquantPass

--- processor.js
+++ processor.js
@@ -2,12 +2,16 @@ function processor(transmission) {
     if (!transmission.includes("::")) {
         throw new Error('Data is invalid ; should contain "::"');
     }
+    if (!transmission.includes(">") && !transmission.includes("<")) {
+        throw new Error('Data is invalid ; should contain ">" and "<"');
+    }
     if (!transmission.includes("<")) {
         throw new Error('Data is invalid ; should contain "<"');
     }
     if (!transmission.includes(">")) {
         throw new Error('Data is invalid ; should contain ">"');
     }
+    
     let parts = transmission.split("::");
     let rawData = parts[1];
     return {
